7dbf095a635109cdbc3d3d9ddd8cc7fe38a4280a
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-01 11:50:39 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <h1>Start</h1>
+  <LayoutHeader />
+  <BookList />
 </template>
 
 <script>
+import LayoutHeader from "./components/LayoutHeader.vue";
+import BookList from "./components/BookList.vue";
+
 export default {
-  components: {},
+  components: { LayoutHeader, BookList },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "@/assets/reset.css";
+import "@/assets/global.css";
 createApp(App).mount("#app");
